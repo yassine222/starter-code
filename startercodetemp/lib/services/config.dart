72ea4a7faebf1f12c7732a2d323a6f9a8877c765
yahya73 <yahya.fhima@esprit.tn>
2024-04-20 10:37:31 +0100

    first

---
+++ startercodetemp/lib/services/config.dart
@@ -0,0 +1,18 @@
+class Config {
+  static const apiUrl = "jobhubrest-main2-production.up.railway.app";
+  static const String loginUrl = "/api/login";
+  static const String signupUrl = "/api/register";
+  static const String jobs = "/api/jobs";
+  static const String agentJobs = "/api/agent/";
+  static const String search = "/api/jobs/search";
+  static const String job = "/api/jobs";
+  static const String profileUrl = "/api/users/";
+  static const String cvUrl = "/api/users/cv";
+  static const String getprofileUrl = "/api/users/";
+  static const String bookmarkUrl = "/api/bookmarks";
+  static const String chatsUrl = "/api/chats";
+  static const String messagingUrl = "/api/messages";
+  static const String applicationUrl = "/api/application";
+  static const String userApplicationsUrl = "/api/application";
+}
+// https://e11a-107-186-235-202.ngrok-free.app
